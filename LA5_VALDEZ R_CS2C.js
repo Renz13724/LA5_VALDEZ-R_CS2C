2ba4f1a7e37a89187b68606f322d94803e9c2ac3
VALDEZ R. <renzv753@gmail.com>
2024-09-22 17:40:51 +0800

    Create LA5_VALDEZ R_CS2C.js

---
+++ LA5_VALDEZ R_CS2C.js
@@ -0,0 +1,13 @@
+let favActorFirstName = "Coco";
+let favActorLastName = "Martin";
+let fullName = favActorFirstName + " " + favActorLastName; 
+let uppercase = fullName.toUpperCase(); 
+let message = "My favorite Actor is " + uppercase + ". He is a talented and handsome actor."; 
+
+message += " His best show is Silicon Valley."; 
+
+console.log("favActorFirstName:", favActorFirstName);
+console.log("favActorLastName:", favActorLastName);
+console.log("fullName:", fullName);
+console.log("uppercase:", uppercase);
+console.log("message:", message); 
